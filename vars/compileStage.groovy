99a56fa4e9035ab6f73461fb535b5bf1204acd87
Mamatha Bandi <mamathabandi1718@gmail.com>
2025-07-19 12:39:19 +0530

    Update compileStage.groovy

--- vars/compileStage.groovy
+++ vars/compileStage.groovy
@@ -2,11 +2,9 @@
 def call() {
     stage('Compile') {
         steps {
-            script {
                 // You can add logging or error handling here too
                 echo "Running compile step..."
                 sh 'mvn compile'
-            }
         }
     }
 }
