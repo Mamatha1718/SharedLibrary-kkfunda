cd8529b602c99c21fc9f0d7447d0713b859d35af
Mamatha Bandi <mamathabandi1718@gmail.com>
2025-07-19 12:46:32 +0530

    Update compileStage.groovy

--- vars/compileStage.groovy
+++ vars/compileStage.groovy
@@ -1,11 +1,7 @@
 // vars/compileStage.groovy
 def call(String tool = 'mvn') {
     stage('Compile') {
-        steps {
-            script {
                 echo "Running ${tool} compile..."
                 sh "${tool} compile"
-            }
-        }
     }
 }
